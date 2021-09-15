dd4ec841311201ff25a23b05bfa32cc9a066cca9
mc <mairis.caune@gmail.com>
2021-09-15 22:23:59 +0300

    starter files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// import React, { component } from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import './App.css';
+import 'tachyons';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Logo />
+      {/*<ImageLinkForm />
+      <DominantColors />*/}
+    </div>
+  );
+}
+
+export default App;
